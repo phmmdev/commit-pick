87129a8e39b3f69f728ea037e1f8cde22808c0e5
phmmdev <phmmdev@gmail.com>
2020-11-25 00:05:06 -0300

    adding shell script to generate N commits to test ability to cherry-pick a range of commits

---
+++ commit-maker.sh
@@ -0,0 +1,22 @@
+
+_COMMITS="$1"
+_DUMP_FILE_NAME="dump-file.txt"
+if [ -z "$_COMMITS" ] 
+then
+	echo "Please set \$_COMMITS"
+ 
+else
+    echo "Generating dump file: $_DUMP_FILE_NAME ..."
+    touch dump-file.txt 
+	
+    echo "Creating $_COMMITS commits..."
+
+    for (( i=0; i<$_COMMITS; i++ ))
+    do  
+        echo "line $i" >> $_DUMP_FILE_NAME
+        git add .
+        git commit -m "adding line $i to file $_DUMP_FILE_NAME"
+    done
+fi
+
+
